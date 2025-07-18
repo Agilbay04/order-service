330d2063d085b9b308768f54f042be571b77938f
LAPTOP-TC685HR4\AGILE <bayuagil04@gmail.com>
2025-07-18 15:22:50 +0700

    change flow create order

--- Domain/Order/Services/ServiceOrder.cs
+++ Domain/Order/Services/ServiceOrder.cs
@@ -19,7 +19,6 @@ namespace OrderService.Domain.Order.Services
         DataContext dbContext,
         OrderRepository orderRepository,
         ProductService productService,
-        IServiceProvider serviceProvider,
         BackgroundTaskQueue _taskQueue,
         IConnectionMultiplexer redis
     ) : IServiceOrder
@@ -28,7 +27,6 @@ namespace OrderService.Domain.Order.Services
         private readonly DataContext _dbContext = dbContext;
         private readonly OrderRepository _orderRepository = orderRepository;
         private readonly ProductService _productService = productService;
-        private readonly IServiceProvider _serviceProvider = serviceProvider;
         private readonly BackgroundTaskQueue _taskQueue = _taskQueue;
         private readonly IConnectionMultiplexer _redis = redis;
 
@@ -61,6 +59,7 @@ namespace OrderService.Domain.Order.Services
                     var productIds = body.OrderItem.Select(o => o.ProductId).ToList();
                     var orderNumber = await GenerateOrderNumberRedis();
                     var listProduct = await _productService.GetProductByIds(productIds);
+                    var (status, remark) = await CheckStockProductAndGetStatus(orderNumber, body.OrderItem, listProduct);
 
                     var order = new Models.Order()
                     {
@@ -68,7 +67,8 @@ namespace OrderService.Domain.Order.Services
                         CustomerName = body.Customer.Name,
                         CustomerAddress = body.Customer.Address,
                         CustomerPhoneNumber = body.Customer.PhoneNumber,
-                        Status = OrderStatusConstant.PENDING,
+                        Status = status,
+                        Remark = remark,
                         SubTotalPrice = body.OrderItem.Sum(o => o.Qty * listProduct.Single(p => p.Id == o.ProductId).Price),
                         OrderDetails = [.. body.OrderItem.Select(o => new OrderDetail()
                         {
@@ -89,21 +89,6 @@ namespace OrderService.Domain.Order.Services
 
                     await transaction.CommitAsync();
 
-                    // Check stock product
-                    await _taskQueue.QueueBackgroundWorkItemAsync(async ct =>
-                    {
-                        using var scope = _serviceProvider.CreateScope();
-                        var scopedDbContext = scope.ServiceProvider.GetRequiredService<DataContext>();
-                        var scopedProductService = scope.ServiceProvider.GetRequiredService<ProductService>();
-                        await CheckStockProduct(
-                            order.Id,
-                            body.OrderItem,
-                            listProduct,
-                            scopedDbContext,
-                            scopedProductService
-                        );
-                    });
-
                     return orderNumber;
                 }
                 catch (Exception ex)
@@ -115,95 +100,54 @@ namespace OrderService.Domain.Order.Services
             });
         }
 
-        private async Task CheckStockProduct(
-            Guid OrderId,
+        private async Task<(string, string)> CheckStockProductAndGetStatus(
+            string orderNumber,
             List<CreateOrderItemDto> orderItems,
-            List<ProductResultDto> listProduct,
-            DataContext dbContext = null,
-            ProductService productService = null
+            List<ProductResultDto> listProduct
         )
         {
-            await dbContext.Database.CreateExecutionStrategy().ExecuteAsync(async () =>
+            string status = OrderStatusConstant.PENDING;
+            string remark;
+
+            var listOutOfStock = new List<string>();
+
+            foreach (var orderItem in orderItems)
             {
-                using var transaction = await dbContext.Database.BeginTransactionAsync();
-
-                try
+                var product = listProduct.Single(p => p.Id == orderItem.ProductId);
+                if (product.Stock < orderItem.Qty)
                 {
-                    var listOutOfStock = new List<string>();
-
-                    var order = await dbContext.Orders
-                        .Where(o => o.Id == OrderId
-                            && o.DeletedAt == null
-                            && o.Status == OrderStatusConstant.PENDING)
-                        .FirstOrDefaultAsync();
-
-                    foreach (var orderItem in orderItems)
-                    {
-                        var product = listProduct.Single(p => p.Id == orderItem.ProductId);
-                        if (product.Stock < orderItem.Qty)
-                        {
-                            listOutOfStock.Add(product.Name);
-                        }
-                    }
-
-                    if (listOutOfStock.Count != 0)
-                    {
-                        order.Status = OrderStatusConstant.REJECTED;
-                        order.Remark = OrderErrorMessage.OrderProductOutOfStock(listOutOfStock);
-                    }
-                    else
-                    {
-                        order.Status = OrderStatusConstant.CONFIRMED;
-                        order.Remark = OrderMessage.ProcessOrderToDelivery(order.OrderNumber);
-
-                        var paramUpdateStock = orderItems.Select(o => new UpdateStockProductDto()
-                        {
-                            ProductId = o.ProductId,
-                            Qty = o.Qty
-                        }).ToList();
-
-                        await productService.UpdateStockProductAsync(paramUpdateStock);
-                    }
-
-                    await dbContext.SaveChangesAsync();
-                    await transaction.CommitAsync();
+                    listOutOfStock.Add(product.Name);
                 }
-                catch (Exception ex)
+            }
+
+            if (listOutOfStock.Count != 0)
+            {
+                status = OrderStatusConstant.REJECTED;
+                remark = OrderErrorMessage.OrderProductOutOfStock(listOutOfStock);
+            }
+            else
+            {
+                status = OrderStatusConstant.CONFIRMED;
+                remark = OrderMessage.ProcessOrderToDelivery(orderNumber);
+
+                var paramUpdateStock = orderItems.Select(o => new UpdateStockProductDto()
                 {
-                    await transaction.RollbackAsync();
-                    _logger.LogError(ex, "Error Check Stock Product: {Message}", ex.Message);
-                    throw;
-                }
-            });
+                    ProductId = o.ProductId,
+                    Qty = o.Qty
+                }).ToList();
+
+                await QueueUpdateStockProduct(paramUpdateStock);
+            }
+
+            return (status, remark);
         }
 
-        private async Task<string> GenerateOrderNumber()
+        private async Task QueueUpdateStockProduct(List<UpdateStockProductDto> param)
         {
-            try
+            await _taskQueue.QueueBackgroundWorkItemAsync(async ct =>
             {
-                var today = DateTime.Now;
-                var datepart = today.ToString("yyMMdd");
-                var prefix = $"ORD{datepart}";
-
-                var lastOrder = await _orderRepository.GetLatestOrderNumber(prefix);
-
-                int lastSequence = 0;
-                if (lastOrder != null)
-                {
-                    var lasSeqString = lastOrder[prefix.Length..];
-                    lastSequence = int.TryParse(lasSeqString, out int seq) ? seq : 0;
-                }
-
-                var newSequence = lastSequence + 1;
-                var newOrderNumber = $"{prefix}{newSequence:D7}";
-
-                return newOrderNumber;
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error Generate Order Number: {Message}", ex.Message);
-                throw;
-            }
+                await _productService.UpdateStockProductAsync(param);
+            });
         }
 
         private async Task<string> GenerateOrderNumberRedis()
